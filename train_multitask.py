56039194d91197d92f5e4bf4ca0f86d26c104d0e
omarsamy1995 <omarsamy1326@gmail.com>
2026-09-07 14:47:52 +0000

    train

--- train_multitask.py
+++ train_multitask.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # تحميل الموديل المطور بـ CBAM
-model = YOLO('yolo11-cbam-pose.yaml')
+model = YOLO('/teamspace/studios/this_studio/ultralytics/ultralytics/cfg/models/11/yolo11-cbam-pose.yaml')
 
 # نقل الأوزان السابقة
 results = model.train(
     data='/teamspace/studios/this_studio/ultralytics/KLYVERO_Dataset/data.yaml',
-    pretrained='/teamspace/studios/this_studio/ultralytics/best.pt',  # مسار أفضل أوزان السابقة
+    pretrained='/teamspace/studios/this_studio/ultralytics/runs/pose/KLYVERO_Engine/yolo11n_body_12kpt-2/weights/best.pt',  # مسار أفضل أوزان السابقة
     epochs=100,
     imgsz=640,
     batch=16,
